5a550ef82b9d1f1c3d9833b8d0b865373a858632
Violette Jacobs <72454626+VioletteJacobs@users.noreply.github.com>
2021-02-14 21:07:02 +0100

    epuration

---
+++ src/componants/contenu/Contenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Contenu.css"
+
+export default function Contenu() {
+    return (
+        <div>
+             <h1>Rejoindre le côté sombre de la Force</h1>
+            <span>Exercice utilisant les hooks en react et la context API.</span>
+            
+        </div>
+    )
+}
